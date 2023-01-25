b36d7096541c848d31d67d21b399fa7db74cc107
XiaoYi <2535131331@qq.com>
2023-01-25 22:39:15 +0800

    add README

--- src/main.cpp
+++ src/main.cpp
@@ -7,38 +7,38 @@ int main(int argc, const char* argv[])
 {
     {
         Regex re("(a|b)*a(b|c)*b(a|c)*");
-        cout << re.match("aabbccccccc") << endl;
-        cout << re.match("aabbccab") << endl << endl;
+        cout << re.match("aabbccccccc") << endl; // true
+        cout << re.match("aabbccab") << endl << endl; // false
     }
     {
         Regex re("(0|1)*(01)(0|1)*(01)(0|1)*");
-        cout << re.match("0000111") << endl;
-        cout << re.match("010000001010111") << endl;
-        cout << re.match("22222333") << endl << endl;
+        cout << re.match("0000111") << endl;  // false
+        cout << re.match("010000001010111") << endl; // true
+        cout << re.match("22222333") << endl << endl; // false
     }
     {
         Regex re("((0|1)*1(0|1)*0(0|1)*0(0|1)*)|((0|1)*0(0|1)*1(0|1)*0(0|1)*)|((0|1)*0(0|1)*0(0|1)*1(0|1)*)");
-        cout << re.match("00000000000000") << endl;
-        cout << re.match("1111111111111111111111110") << endl;
-        cout << re.match("1111111111110111011111111111") << endl << endl;
+        cout << re.match("00000000000000") << endl; //false
+        cout << re.match("1111111111111111111111110") << endl; //false
+        cout << re.match("1111111111110111011111111111") << endl << endl; //true
     }
     {
         Regex re("((0|1)(0|1))*((0|1)0|1)");
-        cout << re.match("111111000") << endl;
-        cout << re.match("10100101") << endl;
-        cout << re.match("111110000010101010100100") << endl;
-        cout << re.match("100010111011111000011") << endl << endl;
+        cout << re.match("111111000") << endl; //false
+        cout << re.match("10100101") << endl; //false
+        cout << re.match("111110000010101010100100") << endl; //true
+        cout << re.match("100010111011111000011") << endl << endl; //true
     }
     {
         Regex re("(\\(|\\))*");
-        cout << re.match("((((((") << endl;
-        cout << re.match("))()()(") << endl;
-        cout << re.match("()()()))s()") << endl << endl; 
+        cout << re.match("((((((") << endl; //true
+        cout << re.match("))()()(") << endl; //true
+        cout << re.match("()()()))s()") << endl << endl; //false
     }
     {
         Regex re("(0|1|2|3|4|5|6|7|8|9)*@qq.com");
-        cout << re.match("2535131331@qq.com") << endl;
-        cout << re.match("22231@qqq.com") << endl;
-        cout << re.match("12312git@qq.com") << endl;
+        cout << re.match("2535131331@qq.com") << endl; // true
+        cout << re.match("22231@qqq.com") << endl; //false
+        cout << re.match("12312git@qq.com") << endl; //false
     }
 }
